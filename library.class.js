476b53c93d705436fab9c6cc4dba53da070dcb75
PC Tech <systech@convertbetter.local>
2022-12-20 08:59:20 +0800

    library

---
+++ library.class.js
@@ -0,0 +1,68 @@
+class Library {
+    subjects = [
+        {   
+            id: 1,
+            name: 'Mathematics'
+        },
+        {   
+            id: 2,
+            name: 'English'
+        },
+        {   
+            id: 3,
+            name: 'Science'
+        }
+        
+    ];
+
+    books = [
+        {
+            subject_id: 1,
+            name: 'Advanced Algebra',
+            author: 'John Redden',
+            status: 'available'
+        },
+        {
+            subject_id: 1,
+            name: 'Calculus',
+            author: 'James Callahan',
+            status: 'unavailable'
+        },
+        {
+            subject_id: 2, 
+            name: 'Don Quixote',
+            author: 'Miguel de Cervantes',
+            status: 'unavailable'
+            
+        },
+        {
+            subject_id: 2, 
+            name: 'Lord of the Rings',
+            author: 'J.R.R. Tolkien',
+            status: 'available'
+            
+        },
+        {
+            subject_id: 3, 
+            name: 'Insectpedia',
+            author: 'Eric. R. Eaton',
+            status: 'available'
+            
+        },
+        {
+            subject_id: 3, 
+            name: 'The Science of Can and Can’t',
+            author: 'Chiara Marletto',
+            status: 'available'
+        }
+    ]
+
+    addSubject(subject) {
+        const subjectName = {
+            id: this.subjects.length + 1,
+            name: subject
+        }
+
+        this.subjects.push(subjectName);
+    }
+}
